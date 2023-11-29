62ef2b2b18f4ad0d0302a9e660d6e010f9c0eb23
Dylanrage <72884716+Dylanrage@users.noreply.github.com>
2023-11-29 22:52:14 +0100

    changed layout for diensten

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,3 +260,96 @@
   new PureCounter();
 
 })()
+
+// Add this JavaScript to handle the modal
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModal");
+var span = document.getElementById("closeModal");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Add this JavaScript to handle the modals
+var modal1 = document.getElementById("myModal1");
+var btn1 = document.getElementById("openModal1");
+var span1 = document.getElementById("closeModal1");
+
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+var modal2 = document.getElementById("myModal2");
+var btn2 = document.getElementById("openModal2");
+var span2 = document.getElementById("closeModal2");
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+// Add this JavaScript to handle the modals
+var modal3 = document.getElementById("myModal3");
+var btn3 = document.getElementById("openModal3");
+var span3 = document.getElementById("closeModal3");
+
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
+
+var modal4 = document.getElementById("myModal4");
+var btn4 = document.getElementById("openModal4");
+var span4 = document.getElementById("closeModal4");
+
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
