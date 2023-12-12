de1866ae2d956e4938c5f0abbda68387f477fa00
Dylanrage <72884716+Dylanrage@users.noreply.github.com>
2023-12-12 18:01:57 +0100

    added some phpmailer thingies

--- forms/contact.php
+++ forms/contact.php
@@ -1,29 +1,48 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './assets/vendor/PHPMailer/src/Exception.php';
+require './assets/vendor/PHPMailer/src/PHPMailer.php';
+require './assets/vendor/PHPMailer/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
+    // Sanitize user input
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
 
-    // Simple validation (you may want to add more thorough validation)
-    if (empty($name) || empty($email) || empty($subject) || empty($message)) {
-        echo "Error: All fields are required.";
-        exit;
+    // Create a PHPMailer instance
+    $mail = new PHPMailer(true);
+
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp-auth.mailprotect.be'; // Update with your SMTP server
+        $mail->SMTPAuth = true;
+        $mail->Username = 'info@podologiesolesolutions.be'; // Update with your SMTP username
+        $mail->Password = 'Ignim5vw%1'; // Update with your SMTP password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 465;
+
+        // Recipients
+        $mail->setFrom($email, $name);
+        $mail->addAddress('info@podologiesolesolutions.be'); // Update with your email address
+
+        // Content
+        $mail->isHTML(false); // Set to true if your message contains HTML
+        $mail->Subject = $subject;
+        $mail->Body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
+
+        // Send the email
+        $mail->send();
+
+        // Redirect or display a thank you message
+        header("Location: thank_you.html");
+        exit();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
-
-    // Send email (you may want to use a library like PHPMailer for better email handling)
-    $to = "info@podologiesolesolutions.be"; // Replace with your email address
-    $headers = "From: $email\r\nReply-To: $email";
-    $mailBody = "Name: $name\nEmail: $email\nSubject: $subject\n\n$message";
-
-    if (mail($to, $subject, $mailBody, $headers)) {
-        echo "Success: Your message has been sent. Thank you!";
-    } else {
-        echo "Error: Something went wrong. Please try again later.";
-    }
-} else {
-    // If the form is not submitted via POST, you can redirect or handle it as needed.
-    echo "Error: Invalid form submission.";
 }
 ?>
