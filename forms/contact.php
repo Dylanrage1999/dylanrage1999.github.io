5f9598e01cf7f4e203bc7671bb1e6946765239fa
Dylanrage <72884716+Dylanrage@users.noreply.github.com>
2023-12-12 18:07:39 +0100

    other php thingies

--- forms/contact.php
+++ forms/contact.php
@@ -2,9 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require './assets/vendor/PHPMailer/src/Exception.php';
-require './assets/vendor/PHPMailer/src/PHPMailer.php';
-require './assets/vendor/PHPMailer/src/SMTP.php';
+require 'vendor/autoload.php'; // Include the Composer autoloader
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Sanitize user input
