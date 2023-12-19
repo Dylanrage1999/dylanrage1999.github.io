c167e7b8bac9396aaf71793d67219dcc83245edb
Dylanrage <72884716+Dylanrage@users.noreply.github.com>
2023-12-19 17:48:31 +0100

    Update contact.php
    added code for using amazon ses SMTP server

--- forms/contact.php
+++ forms/contact.php
@@ -1,48 +1,47 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 
-require '/PHPMAILER/src/Exception.php';
-require '/PHPMAILER/src/PHPMailer.php';
-require '/PHPMAILER/src/SMTP.php';
+require '/vendor/autoload.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Sanitize user input
-    $name = htmlspecialchars($_POST["name"]);
-    $email = htmlspecialchars($_POST["email"]);
-    $subject = htmlspecialchars($_POST["subject"]);
-    $message = htmlspecialchars($_POST["message"]);
+use Aws\SES\SESClient;
 
-    // Create a PHPMailer instance
-    $mail = new PHPMailer(true);
+try {
+    $sesClient = new SESClient([
+        'key' => 'AKIAZZEU2G55ZXR4GFUO',    //Vervang de waarde van YOUR_API_KEY_ID door je API-sleutel-ID van Amazon SES.
+        'secret' => 'BFDacKHLb5bp6LP7I44n4a6FKDBGaoOrL7tNp0xPhsPq',    //Vervang de waarde van YOUR_SECRET_ACCESS_KEY door je geheime toegangssleutel van Amazon SES.
+        'region' => 'eu-north-1'
+    ]);
 
-    try {
-        // Server settings
-        $mail->isSMTP();
-        $mail->Host = 'smtp-auth.mailprotect.be'; // Update with your SMTP server
-        $mail->SMTPAuth = true;
-        $mail->Username = 'dylan.rage@icloud.com'; // Update with your SMTP username
-        $mail->Password = 'Ignim5vw%1'; // Update with your SMTP password
-        $mail->SMTPSecure = 'false'; //was tls
-        $mail->Port = 465;
 
-        // Recipients
-        $mail->setFrom($email, $name);
-        $mail->addAddress('info@podologiesolesolutions.be'); // Update with your email address
 
-        // Content
-        $mail->isHTML(false); // Set to true if your message contains HTML
-        $mail->Subject = $subject;
-        $mail->Body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
+    //Zorg ervoor dat je het sender email address in from hebt geconfigureerd.
+    //Zorg ervoor dat je de recipient email address in to hebt geconfigureerd.
 
-        // Send the email
-        $mail->send();
+    $from = 'from@example.com'; // Update with your sender email address
+    $to = 'info@podologiesolesolutions.be'; // Update with the recipient email address
+    $subject = $subject;
+    $body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
 
-        // Redirect or display a thank you message
-        header("Location: thank_you.html");
-        exit();
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    $result = $sesClient->sendEmail([
+        'Source' => $from,
+        'Destination' => [
+            'ToAddresses' => [
+                $to
+            ]
+        ],
+        'Message' => [
+            'Subject' => $subject,
+            'Body' => [
+                'Html' => $body
+            ]
+        ]
+    ]);
+
+    if ($result->getStatusCode() == 200) {
+        echo "De e-mail is verzonden.";
+    } else {
+        echo "Er is een fout opgetreden bij het verzenden van de e-mail.";
     }
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 ?>
